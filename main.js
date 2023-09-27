3e579ad8d143e609df117deb66a4ceaf2d6036d7
khetlyn06 <107417239+khetlyn06@users.noreply.github.com>
2023-09-27 11:37:48 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
-// Pom ----------------------------------------
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-const ListaDeTeclas = document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-//enquanto
-while (contador < ListaDeTeclas.length) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
 
- ListaDeTeclas[contador].onclick = tocaSomPom;
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
 
- contador = contador + 1;
+tecla.onkeydown = function () {
+    tecla.classList.add('ativa');
+}
 
- console.log(contador);
+ tecla.onkeyup = function (){
+    tecla.classList.remove('ativa');
+ }
 
 }
